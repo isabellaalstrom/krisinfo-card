874c0b228f33a770f2f435d09c83dc1449b2645a
Thomas Lovén <thomasloven@gmail.com>
2020-09-18 11:01:47 +0200

    Remove card-tools dependency

--- krisinfo-card.js
+++ krisinfo-card.js
@@ -1,155 +1,151 @@
-customElements.whenDefined('card-tools').then(() => {
-  let cardTools = customElements.get('card-tools');
-    
-  class KrisinfoCard extends cardTools.LitElement {
-    
-    setConfig(config) {
+const LitElement = customElements.get('home-assistant-main')
+? Object.getPrototypeOf(customElements.get('home-assistant-main'))
+: Object.getPrototypeOf(customElements.get('hui-view'));
+const html = LitElement.prototype.html;
+const css = LitElement.prototype.css;
+
   
-      if (!config.entity) {
-        throw new Error('Please define entity');
-      }
-      this.config = config;
+class KrisinfoCard extends LitElement {
+  
+  setConfig(config) {
+
+    if (!config.entity) {
+      throw new Error('Please define entity');
     }
-    
-    render(){
-      if (!this.entity)
-        return html`
-        <hui-warning>
-          ${this._hass.localize("ui.panel.lovelace.warning.entity_not_found",
-            "entity",
-            this.config.entity
-          )}
-        </hui-warning>
-        `
-      return cardTools.LitHtml
-      `
-        ${this._renderStyle()}
-        ${this.config.only_alerts == false || this.config.only_alerts == null
-          ? cardTools.LitHtml
-          `<ha-card>
-            <div class="header">
-              <div class="name">
-                ${this.header}
-              </div>
-            </div>
-            ${this.messages.length == 0
-              ? cardTools.LitHtml`<p>Inga meddelanden just nu</p>`
-              : this.messages.map(message => 
-                cardTools.LitHtml`<div class="message">
-                        <span>${message.SenderName}</span>
-                          </br><span>Skickat: ${message.Published}</span>
-                          <h3>${message.Event == "Alert" ? cardTools.LitHtml`<ha-icon id="state-icon" icon="mdi:alert"></ha-icon>` : ''}${message.Headline}</h3>
-                        <p class="">${message.Message}</p>
-                        <a target="_blank" href="${message.Web}">Läs mer</a>
-                      </div>
-                `)}
-            </ha-card>`
-          : // if only alerts
-          cardTools.LitHtml
-          `
-          ${this.state == "Alert" ?
-            cardTools.LitHtml
-            `
-              <ha-card>
-                <div class="header">
-                  <div class="name">
-                    ${this.state} - ${this.header}
-                  </div>
-                </div>
-                ${this.messages.length == 0
-                  ? cardTools.LitHtml`<p>Inga meddelanden just nu</p>`
-                  : this.messages.map(message => 
-                    cardTools.LitHtml`
-                    ${message.Event == "Alert" ?
-                      cardTools.LitHtml`<div class="message">
-                              <span>${message.SenderName}</span>
-                                </br><span>Skickat: ${message.Published}</span>
-                                <h3>${message.Event == "Alert" ? cardTools.LitHtml`<ha-icon id="state-icon" icon="mdi:alert"></ha-icon>` : ''}${message.Headline}</h3>
-                              <p class="">${message.Message}</p>
-                              <a target="_blank" href="${message.Web}">Läs mer</a>
-                            </div>
-                      `
-                    : ''}`
-                    )
-                
-                  }
-              </ha-card>
-            `
-            : ''
-          }
-          `
-        }
-      `;
-    }    
-  
-    _renderStyle() {
-        return cardTools.LitHtml
-        `
-          <style>
-            ha-card {
-              padding: 16px;
-            }
-            .header {
-              padding: 0;
-              @apply --paper-font-headline;
-              line-height: 40px;
-              color: var(--primary-text-color);
-              padding: 4px 0 12px;
-            }
-            .message {
-              padding-bottom: 1em;
-            }
-            h3 {
-              margin-top: 10px;
-              margin-bottom: 0;
-            }
-            p {
-              margin-top: 7px;
-            }
-            .card {
-              padding: 1em;
-              padding-top: 0;
-            }
-            .state {
-              display: block;
-              margin: auto;
-              text-align: center;
-            }
-            #state-icon {
-              color: red;
-              padding-right: 7px;
-            }
-          </style>
-        `;
-      }
-    
-    set hass(hass) {
-      this._hass = hass;
-      this.entity = this.config.entity in hass.states ? hass.states[this.config.entity] : null;
-      if(this.entity)
-      {
-        this.header = this.entity.attributes.friendly_name;
-        this.messages = this.entity.attributes.messages;
-        this.state = this.entity.state
-      }
-  
-      this.requestUpdate();
-    }
-  
-  
-  
-    // @TODO: This requires more intelligent logic
-    getCardSize() {
-      return 3;
-    }
+    this.config = config;
   }
   
-  customElements.define('krisinfo-card', KrisinfoCard);
-  });
+  render(){
+    if (!this.entity)
+      return html`
+      <hui-warning>
+        ${this._hass.localize("ui.panel.lovelace.warning.entity_not_found",
+          "entity",
+          this.config.entity
+        )}
+      </hui-warning>
+      `
+    return html
+    `
+      ${this._renderStyle()}
+      ${this.config.only_alerts == false || this.config.only_alerts == null
+        ? html
+        `<ha-card>
+          <div class="header">
+            <div class="name">
+              ${this.header}
+            </div>
+          </div>
+          ${this.messages.length == 0
+            ? html`<p>Inga meddelanden just nu</p>`
+            : this.messages.map(message => 
+              html`<div class="message">
+                      <span>${message.SenderName}</span>
+                        </br><span>Skickat: ${message.Published}</span>
+                        <h3>${message.Event == "Alert" ? html`<ha-icon id="state-icon" icon="mdi:alert"></ha-icon>` : ''}${message.Headline}</h3>
+                      <p class="">${message.Message}</p>
+                      <a target="_blank" href="${message.Web}">Läs mer</a>
+                    </div>
+              `)}
+          </ha-card>`
+        : // if only alerts
+        html
+        `
+        ${this.state == "Alert" ?
+          html
+          `
+            <ha-card>
+              <div class="header">
+                <div class="name">
+                  ${this.state} - ${this.header}
+                </div>
+              </div>
+              ${this.messages.length == 0
+                ? html`<p>Inga meddelanden just nu</p>`
+                : this.messages.map(message => 
+                  html`
+                  ${message.Event == "Alert" ?
+                    html`<div class="message">
+                            <span>${message.SenderName}</span>
+                              </br><span>Skickat: ${message.Published}</span>
+                              <h3>${message.Event == "Alert" ? html`<ha-icon id="state-icon" icon="mdi:alert"></ha-icon>` : ''}${message.Headline}</h3>
+                            <p class="">${message.Message}</p>
+                            <a target="_blank" href="${message.Web}">Läs mer</a>
+                          </div>
+                    `
+                  : ''}`
+                  )
+              
+                }
+            </ha-card>
+          `
+          : ''
+        }
+        `
+      }
+    `;
+  }    
+
+  _renderStyle() {
+      return css
+      `
+        <style>
+          ha-card {
+            padding: 16px;
+          }
+          .header {
+            padding: 0;
+            @apply --paper-font-headline;
+            line-height: 40px;
+            color: var(--primary-text-color);
+            padding: 4px 0 12px;
+          }
+          .message {
+            padding-bottom: 1em;
+          }
+          h3 {
+            margin-top: 10px;
+            margin-bottom: 0;
+          }
+          p {
+            margin-top: 7px;
+          }
+          .card {
+            padding: 1em;
+            padding-top: 0;
+          }
+          .state {
+            display: block;
+            margin: auto;
+            text-align: center;
+          }
+          #state-icon {
+            color: red;
+            padding-right: 7px;
+          }
+        </style>
+      `;
+    }
   
-  window.setTimeout(() => {
-    if(customElements.get('card-tools')) return;
-    customElements.define('krisinfo-card', class extends HTMLElement{
-      setConfig() { throw new Error("Can't find card-tools. See https://github.com/thomasloven/lovelace-card-tools");}
-    });
-  }, 2000);
+  set hass(hass) {
+    this._hass = hass;
+    this.entity = this.config.entity in hass.states ? hass.states[this.config.entity] : null;
+    if(this.entity)
+    {
+      this.header = this.entity.attributes.friendly_name;
+      this.messages = this.entity.attributes.messages;
+      this.state = this.entity.state
+    }
+
+    this.requestUpdate();
+  }
+
+
+
+  // @TODO: This requires more intelligent logic
+  getCardSize() {
+    return 3;
+  }
+}
+
+customElements.define('krisinfo-card', KrisinfoCard);
